353617cd0dc24e7b0dec97bc1a7f6da8758da0fd
millisami <millisami@gmail.com>
2010-05-03 17:27:09 +0545

    session_store removed from .gitignore

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+ActionController::Base.session = {
+    :key         => '_right_rails_session',
+    :secret      => 'beb208f0d72f4ffb85eb6f5f0aa9e63ca659f0f68d453835888989b91484df792fc1743c11f43a5bc05c0eb3a66c41bcffa07befb9550ae2522e3614adae81e3'
+}
